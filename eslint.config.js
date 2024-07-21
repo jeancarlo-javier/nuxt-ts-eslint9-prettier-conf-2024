afa2350638865184970a77137916410e5419dfb1
Jeancarlo Javier <naphisto43@gmail.com>
2024-07-21 15:44:52 -0500

    vue files - vue/no-unused-components

--- eslint.config.js
+++ eslint.config.js
@@ -13,6 +13,12 @@ export default [
   ...pluginVue.configs["flat/essential"],
   {files: ["**/*.vue"], languageOptions: {parserOptions: {parser: tseslint.parser}}},
   {
+    files: ["**/*.vue"],
+    rules: {
+      "vue/no-unused-components": "error",
+    }
+  },
+  {
     ignores: ['node_modules', 'dist', 'public', '.nuxt']
   },
   eslintConfigPrettier
