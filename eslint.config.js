afd0bda1e0d98548ec98c192e3f7a7690299ca34
Jeancarlo Javier <jeancarlojavier43@gmail.com>
2024-07-21 15:17:48 -0500

    Typescript dep

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginVue from "eslint-plugin-vue";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 
 export default [
@@ -11,4 +12,8 @@ export default [
   ...tseslint.configs.recommended,
   ...pluginVue.configs["flat/essential"],
   {files: ["**/*.vue"], languageOptions: {parserOptions: {parser: tseslint.parser}}},
+  {
+    ignores: ['node_modules', 'dist', 'public', '.nuxt']
+  },
+  eslintConfigPrettier
 ];
