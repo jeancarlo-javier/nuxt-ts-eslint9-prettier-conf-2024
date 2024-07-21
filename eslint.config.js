960bd65dc2ccfa5b2d478402640fbb22c33a6cf7
Jeancarlo Javier <jeancarlojavier43@gmail.com>
2024-07-21 15:05:56 -0500

    Fix Extensions for Eslint Config

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,6 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginVue from "eslint-plugin-vue";
-import eslintConfigPrettier from "eslint-config-prettier";
 
 
 export default [
@@ -12,14 +11,4 @@ export default [
   ...tseslint.configs.recommended,
   ...pluginVue.configs["flat/essential"],
   {files: ["**/*.vue"], languageOptions: {parserOptions: {parser: tseslint.parser}}},
-  eslintConfigPrettier,
-  {
-    files: ["**/*.vue"],
-    rules: {
-      "vue/multi-word-component-names": "off",
-    },
-  },
-  {
-    ignores: ['node_modules', 'dist', 'public', '.nuxt']
-  },
 ];
